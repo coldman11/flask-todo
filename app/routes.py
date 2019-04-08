ade0b2887994f3d874339b9d4b415fa9b1299fe0
Michal <miketopolsky1@interia.pl>
2019-04-08 23:15:00 +0200

    configured styles

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    todos = ['elo', 'siema', 'cze']
+    return render_template('index.html', todos=todos)
