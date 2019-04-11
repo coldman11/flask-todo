1ad26e60ff2e634d7e241bebb6e7dd9340a360bb
Michal <miketopolsky1@interia.pl>
2019-04-11 22:12:52 +0200

    added adding task functionality

--- app.py
+++ app.py
@@ -1,41 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
-
 
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/repos/g3/flask-todo/todo.db'
-
 db = SQLAlchemy(app)
-
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
-
 @app.route('/')
 @app.route('/index')
 def index():
-    todos = [
-        {
-            'text':'eat',
-            'completed': False
-        },
-        {
-            'text': 'sleep',
-            'completed': False
-        },
-        {
-            'text': 'code',
-            'completed': False
-        }
-    ]
+    todos = Todo.query.all()
     return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = Todo(text=request.form['todoitem'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
